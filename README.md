821bd6d836cce7f8738b61e4c7358fbe04518392
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-01-24 14:55:08 +0200

    -- initial

---
+++ README.md
@@ -0,0 +1,8 @@
+### Scala 99 Problems solved in a pure functional way
+
+
+### other solution found after
+
+- (initial link)[https://aperiodic.net/phil/scala/s-99/]
+- (1)[https://github.com/mwttg-code-katas/scala-99-problems]
+- 
--- src/main/scala/problems/P01.scala
+++ src/main/scala/problems/P01.scala
@@ -1,12 +1,34 @@
 package problems
 
-import org.scalatest.Succeeded
 import tools.Sandbox
+
+import scala.annotation.tailrec
+
+/** https://aperiodic.net/phil/scala/s-99/#p01 */
+object P01 {
+
+  /** the function is partial, it fails on empty list */
+  @tailrec
+  def last[A](as: List[A]): A = as match {
+    case Nil      => throw new NoSuchElementException
+    case h :: Nil => h
+    case _ :: t   => last(t)
+  }
+
+}
 
 class P01 extends Sandbox {
 
-  test("1") {
-    Succeeded
+  import P01._
+
+  test("should find the last element") {
+    val data = List(1, 1, 2, 3, 5, 8)
+    last(data) shouldBe 8
+  }
+
+  test("should throw an Exception") {
+    val empty = List.empty[Int]
+    a[NoSuchElementException] should be thrownBy last(empty)
   }
 
 }
