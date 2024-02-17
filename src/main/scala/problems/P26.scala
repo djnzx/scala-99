e76db4b1a04de0b9286504052050f78dccbd09c8
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-02-17 15:53:14 +0200

    - 26, 27

---
+++ src/main/scala/problems/P26.scala
@@ -0,0 +1,102 @@
+package problems
+
+import tools.Sandbox
+
+/** Generate the combinations of K distinct objects chosen from the N elements of a list
+  * {{{
+  *     n!
+  * -----------
+  * k! * (n-k)!
+  *
+  * 3 of 4 = 4
+  * 3 of 5 = 10
+  *
+  *     5!
+  * ----------- = 10
+  * 3! * (5-3)!
+  * }}}
+  * actual signature should be {{{Seq[A] => Seq[Set[A]]}}}
+  *
+  * [[https://aperiodic.net/phil/scala/s-99/#p26]]
+  */
+object P26 {
+
+  private def make[A](as: List[A])(f: List[A] => List[List[A]]): List[List[A]] = as match {
+    case Nil      => List.empty
+    case _ :: ast =>
+      val wX = f(as)
+      val woX = make(ast)(f)
+      wX ::: woX
+  }
+
+  private def f[A](n: Int)(as: List[A]): List[List[A]] = as match {
+    case Nil      => Nil
+    case h :: ast => pickN2(n, ast).map(xs => h :: xs)
+  }
+
+  def pickN2[A](n: Int, as: List[A]): List[List[A]] = n match {
+    case 0 => List(List.empty)
+    case n => make(as) { f(n - 1) }
+  }
+
+  // combinations w/permutations
+  // List(a, b) != List(b, a)
+  // pick (2, abc) => ab, ac, ba, bc, ca
+  def pickX[A](n: Int, xs: List[A]): List[List[A]] = n match {
+    case 0 => List(List.empty) // nothing to add here
+    case n =>
+      xs.flatMap { x =>
+        val xsNoX = xs.filter(_ != x)          // given list without that element
+        val comb1 = pickX(n - 1, xsNoX)        // combinations N-1 for xs - x
+        val comb1wX = comb1.map(ys => x :: ys) // with element added
+        comb1wX
+      }
+  }
+
+  // combinations w.o./permutations
+  // List(a, b) == List(b, a)
+  // pick (2, abc) => ab, ac, bc
+  def pickN[A](n: Int, xs: List[A]): List[List[A]] = (n, xs) match {
+    case (0, _)        => List(List.empty) // we are done, ok
+    case (_, Nil)      => List()           // we are done
+    case (n, x :: nox) =>
+      val wX = pickN(n - 1, nox).map(ys => x :: ys)
+      val woX = pickN(n, nox)
+      wX ::: woX
+  }
+
+}
+
+class P26 extends Sandbox {
+  import P26._
+
+  test("w. permutations") {
+    Seq(
+      (2, "abc") -> Seq("ab", "ac", "ba", "bc", "ca", "cb"),
+      (3, "abc") -> Seq("abc", "acb", "bac", "bca", "cab", "cba")
+    ).foreach { case ((n, xs), expectedL) =>
+      val expected = expectedL.map(_.toList).toList
+      val actual = pickX(n, xs.toList)
+
+      actual shouldBe expected
+    }
+  }
+
+  test("w.o. permutations") {
+    Seq(
+      (1, "abc")   -> Seq("a", "b", "c"),
+      (2, "abc")   -> Seq("ab", "ac", "bc"),
+      (3, "abc")   -> Seq("abc"),
+      (3, "abcd")  -> Seq("abc", "abd", "acd", "bcd"),
+      (4, "abcde") -> Seq("abcd", "abce", "abde", "acde", "bcde"),
+      (3, "abcde") -> Seq("abc", "abd", "abe", "acd", "ace", "ade", "bcd", "bce", "bde", "cde")
+    ).foreach { case ((n, xs), expectedL) =>
+      val expected = expectedL.map(_.toList).toList
+      val actual1 = pickN2(n, xs.toList)
+      val actual2 = pickN(n, xs.toList)
+
+      actual1 shouldBe expected
+      actual2 shouldBe expected
+    }
+  }
+}
---
+++ src/main/scala/problems/P27.scala
@@ -0,0 +1,75 @@
+package problems
+
+import pprint.pprintln
+import tools.Sandbox
+
+/** [[https://aperiodic.net/phil/scala/s-99/#p27]] */
+object P27 {
+  import P26.{pickN => combinations}
+
+  implicit class ListOps[A](private val as: List[A]) extends AnyVal {
+    def --(bs: List[A]): List[A] = as.filterNot(bs.contains)
+  }
+
+  def group3[A](as: List[A]) =
+    for {
+      xs <- combinations(2, as)  // groups of 2
+      woA = as -- xs
+      ys <- combinations(3, woA) // groups of 3 w.o original ones
+      zs = woA -- ys // group of N-2-3
+    } yield List(xs, ys, zs)
+
+  //                                        variants
+  //                                             groups
+  //                                                  group
+  def groupN1[A](ns: List[Int], la: List[A]): List[List[List[A]]] =
+    ns match {
+      case Nil     => List(Nil)
+      case n :: ns =>
+        require((ns.sum + n) == la.length, "Sum of the list should equal to number of the list items")
+        combinations(n, la) // possible groups of N items
+          .flatMap { cs: List[A] => // c       - subgroup
+            groupN1(ns, la -- cs)   // la -- c - rest of the team, build recursively
+              .map { cs :: _ } // attach subgroup
+          }
+    }
+
+  def groupN2[A](ns: List[Int], la: List[A]): List[List[List[A]]] = ns match {
+    case Nil     => List(Nil)
+    case n :: ns =>
+      for {
+        c <- combinations(n, la)
+        g <- groupN2(ns, la -- c).map { c :: _ }
+      } yield g
+  }
+
+}
+
+class P27 extends Sandbox {
+  import P27._
+
+  test("1") {
+    val origin = "abcde".toList
+    val expected = List(
+      List(List('a', 'b'), List('c', 'd', 'e')),
+      List(List('a', 'c'), List('b', 'd', 'e')),
+      List(List('a', 'd'), List('b', 'c', 'e')),
+      List(List('a', 'e'), List('b', 'c', 'd')),
+      List(List('b', 'c'), List('a', 'd', 'e')),
+      List(List('b', 'd'), List('a', 'c', 'e')),
+      List(List('b', 'e'), List('a', 'c', 'd')),
+      List(List('c', 'd'), List('a', 'b', 'e')),
+      List(List('c', 'e'), List('a', 'b', 'd')),
+      List(List('d', 'e'), List('a', 'b', 'c'))
+    )
+    val groups = List(2, 3)
+    val actual1 = groupN1(groups, origin)
+    val actual2 = groupN2(groups, origin)
+
+    actual2.foreach(x => pprintln(x))
+
+    actual1 shouldBe expected
+    actual2 shouldBe expected
+  }
+
+}
