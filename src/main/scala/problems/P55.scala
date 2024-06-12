d665d442019cf1ccb585ebbc69550c686b59c4ae
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-06-12 23:24:09 +0300

    - 59 idea

--- src/main/scala/problems/P55.scala
+++ src/main/scala/problems/P55.scala
@@ -1,5 +1,6 @@
 package problems
 
+import cats.implicits.catsSyntaxTuple2Semigroupal
 import pprint.log
 import scala.math.Ordered.orderingToOrdered
 import tools.Sandbox
@@ -11,6 +12,10 @@ import tools.Sandbox
 object P55 {
 
   sealed trait Tree[+A] {
+    def size: Int = this match {
+      case End           => 0
+      case Node(_, l, r) => 1 + l.size + r.size
+    }
     def mirror: Tree[A] = this match {
       case Node(value, l, r) => Node(value, r, l)
       case t @ End           => t
@@ -32,11 +37,12 @@ object P55 {
     def isHeightBalanced: Option[Int] = this match {
       case End           => Some(0)
       case Node(_, l, r) =>
-        (l.isHeightBalanced, r.isHeightBalanced) match {
-          case (Some(lh), Some(rh)) => Some(lh -> rh).filter { case (lh, rh) => isBalanced(lh, rh) }.map(_ => lh max rh).map(_ + 1)
-          case _                    => None
-        }
+        (l.isHeightBalanced, r.isHeightBalanced).tupled
+          .filter { case (lh, rh) => isBalanced(lh, rh) }
+          .map { case (lh, rh) => lh max rh }
+          .map(_ + 1)
     }
+
     // BST add note to help compiler due to the fact B in the contravariant position
     def add[B >: A](x: B)(implicit ev: Ordering[B]): Tree[B] = this match {
       case End                            => Node(x)
@@ -95,11 +101,71 @@ object P55 {
         .flatMap(x => mkCompletelyBalanced(value, x))
         .filter(_.isSymmetric)
 
-    /** 59. is a bit relaxed than completely balanced */
-    def mkHeightBalanced[A](value: A, n: Int): List[Tree[A]] = n match {
-      case 0 => List(End)
-      case 1 => List(Node(value))
-      case n => ??? // TODO: generate all and check criteria
+    /** 59.B. given N (Nodes count) is a bit relaxed than completely balanced */
+    def mkHeightBalancedN[A](value: A, n: Int): LazyList[Tree[A]] = n match {
+      case 0 => LazyList(End)
+      case 1 => LazyList(Node(value))
+      case n =>
+        LazyList
+          .from(0 to (n - 1)) // left sizes
+          .flatMap { l =>
+            val r = n - 1 - l           // right sizes
+            mkHeightBalancedN(value, l) // all left sub-trees
+              .flatMap { lt =>
+                mkHeightBalancedN(value, r)       // all right sub trees
+                  .map(rt => Node(value, lt, rt)) // combine
+                  .filter(_.isHeightBalanced.isDefined) // check if balanced
+              }
+          }
+    }
+
+    /** 59.A. */
+    // TODO: it hangs h > 5, memoize!
+    def mkHeightBalanced[A](value: A, h: Int): LazyList[Tree[A]] = {
+
+      def mk(h: Int, cache0: Map[Int, LazyList[Tree[A]]]): (LazyList[Tree[A]], Map[Int, LazyList[Tree[A]]]) =
+        h match {
+          case 0 =>
+            cache0.get(h) match {
+              case Some(xx) => xx -> cache0
+              case None     =>
+                val xx = LazyList(End)
+                xx -> (cache0 ++ Map(h -> xx))
+            }
+          case 1 =>
+            cache0.get(h) match {
+              case Some(xx) => xx -> cache0
+              case None     =>
+                val xx = LazyList(Node(value))
+                xx -> (cache0 ++ Map(h -> xx))
+            }
+          case _ =>
+            val (h1ts, cache1p) = cache0.get(h - 1) match {
+              case Some(xx) => xx -> cache0
+              case None     =>
+                val (xx, cacheA) = mk(h - 1, cache0)
+                xx -> (cache0 ++ cacheA)
+            }
+            pprint.log((h, h - 1, cache1p))
+            val cache1 = cache0 ++ cache1p
+
+            val (h2ts, cache2p) = cache1.get(h - 2) match {
+              case Some(xx) => xx -> cache1
+              case None     =>
+                val (xx, cacheA) = mk(h - 2, cache1)
+                xx -> (cache1 ++ cacheA)
+            }
+            pprint.log((h, h - 2, cache2p))
+
+            val tt = h1ts.flatMap(lt => h1ts.map(rt => Node(value, lt, rt))) ++
+              h1ts.flatMap(lt => h2ts.map(rt => Node(value, lt, rt))) ++
+              h2ts.flatMap(lt => h1ts.map(rt => Node(value, lt, rt)))
+
+            val cache3 = Map(h -> tt)
+            tt -> (cache1 ++ cache2p ++ cache3)
+        }
+
+      mk(h, Map.empty)._1
     }
 
     def fromSeq[A: Ordering](xs: Seq[A]): Tree[A] =
--- src/main/scala/problems/P59.scala
+++ src/main/scala/problems/P59.scala
@@ -8,25 +8,59 @@ object P59 {}
 class P59 extends Sandbox {
   import P55._
 
-  test("all height balanced trees") {
+  /*
+
+     o          o           o            o               o            o       |
+    / \       /   \       /   \        /   \           /   \        /   \     |
+   o   o     o     o     o     o     o       o       o       o     o      o   |
+      / \     \     \     \   /     /         \     /      /      / \         |
+     o   o     o     o     o o     o           o   o     a       o   o        |
+
+   */
+  test("all height balanced trees with 5 Nodes") {
     val data = Table(
       inHeader,
-      0,
-      1,
-      2,
-      3,
-      4,
       5
     )
 
-    val x = Tree.mkHeightBalanced("a", 4)
-    pprint.log(x)
+    forAll(data) { n =>
+      val t = Tree.mkHeightBalancedN("a", n)
+      pprint.log(n -> t, showFieldNames = false)
+    }
+  }
 
-//
-//    forAll(data) { n =>
-//      val t = Tree.heightBalanced("a", 9)
-//      log(n -> t)
-//    }
+  /*
+   h  | n elements
+ -----|------------------
+   0  | 0
+   1  | 1..1     n < 2^1,    1 ≤ n < 2
+   2  | 2..3     n < 2^2,    2 ≤ n < 4
+   3  | 4..7     n < 2^3,    4 ≤ n < 8
+   4  | 7..15    n < 2^4,    7 ≤ n < 16
+   5  | 12..31   n < 2^5,   12 ≤ n < 32
+
+   */
+  test("all height balanced trees of height") {
+    val data = Table(
+      inHeader,
+//      0, // 1
+//      1, // 1
+//      2, // N(h-1)×N(h-2) + N(h-2)×N(h-1) + N(h-1)×N(h-1) = 1 + 1 + 1 = 3
+//      3, // 3×3 + 3 + 3 = 15
+//      4,
+//      5,
+      6,
+//      7,
+    )
+
+    forAll(data) { h =>
+      val ts = Tree.mkHeightBalanced("a", h)
+      pprint.log(h -> ts, showFieldNames = false, width = 1000)
+
+      val sizes = ts.map(_.size).distinct.sorted
+      pprint.log(h -> (sizes.head, sizes.last))
+
+    }
   }
 
 }
