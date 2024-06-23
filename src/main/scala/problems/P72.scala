0a5f1cf200243e8b40b0376e71757faa5a680ed8
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-06-23 17:01:22 +0300

    -- P72

---
+++ src/main/scala/problems/P72.scala
@@ -0,0 +1,36 @@
+package problems
+
+import tools.Sandbox
+
+/** https://aperiodic.net/pip/scala/s-99/#p72
+  */
+object P72 {
+
+  import P70._
+
+  def postOrder[A](t: MTree[A]): List[A] =
+    t.children.flatMap(postOrder) ::: List(t.value)
+
+}
+
+class P72 extends Sandbox {
+
+  import P70._
+  import P72._
+
+  val t0 = MTree(
+    'a',
+    List(
+      MTree('f', List(MTree('g'))),
+      MTree('c'),
+      MTree('b', List(MTree('d'), MTree('e')))
+    )
+  )
+
+  val expected = "gfcdeba".toList
+
+  test("postOrder") {
+    postOrder(t0) shouldBe expected
+  }
+
+}
